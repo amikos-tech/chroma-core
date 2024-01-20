f0503c34808e687a97520e6657e54bcfc9b7f82c
Trayan Azarov <trayan.azarov@amikos.tech>
2024-01-20 17:05:34 +0200

    feat: Added TypedDict to batch_utils + example usage
    Result of a community questions

--- chromadb/utils/batch_utils.py
+++ chromadb/utils/batch_utils.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple, List
+from typing import Optional, Tuple, List, TypedDict
 from chromadb.api import BaseAPI
 from chromadb.api.types import (
     Documents,
@@ -6,6 +6,46 @@ from chromadb.api.types import (
     IDs,
     Metadatas,
 )
+
+
+class Batch(TypedDict):
+    ids: IDs
+    embeddings: Optional[Embeddings]
+    metadatas: Optional[Metadatas]
+    documents: Optional[Documents]
+
+
+def split_large_batch(
+    api: BaseAPI,
+    ids: IDs,
+    embeddings: Optional[Embeddings] = None,
+    metadatas: Optional[Metadatas] = None,
+    documents: Optional[Documents] = None,
+) -> List[Batch]:
+    _batches: List[Batch] = []
+    if len(ids) > api.max_batch_size:
+        for i in range(0, len(ids), api.max_batch_size):
+            _batches.append(
+                Batch(
+                    ids=ids[i : i + api.max_batch_size],
+                    embeddings=embeddings[i : i + api.max_batch_size]
+                    if embeddings
+                    else None,
+                    metadatas=metadatas[i : i + api.max_batch_size]
+                    if metadatas
+                    else None,
+                    documents=documents[i : i + api.max_batch_size]
+                    if documents
+                    else None,
+                )
+            )
+    else:
+        _batches.append(
+            Batch(
+                ids=ids, embeddings=embeddings, metadatas=metadatas, documents=documents
+            )
+        )
+    return _batches
 
 
 def create_batches(
@@ -18,17 +58,14 @@ def create_batches(
     _batches: List[
         Tuple[IDs, Embeddings, Optional[Metadatas], Optional[Documents]]
     ] = []
-    if len(ids) > api.max_batch_size:
-        # create split batches
-        for i in range(0, len(ids), api.max_batch_size):
-            _batches.append(
-                (  # type: ignore
-                    ids[i : i + api.max_batch_size],
-                    embeddings[i : i + api.max_batch_size] if embeddings else None,
-                    metadatas[i : i + api.max_batch_size] if metadatas else None,
-                    documents[i : i + api.max_batch_size] if documents else None,
-                )
-            )
-    else:
-        _batches.append((ids, embeddings, metadatas, documents))  # type: ignore
+    _typed_batches = split_large_batch(api, ids, embeddings, metadatas, documents)
+    for batch in _typed_batches:
+        _batches.append(
+            (
+                batch["ids"],
+                batch["embeddings"],
+                batch["metadatas"],
+                batch["documents"],
+            )  # type: ignore
+        )
     return _batches
